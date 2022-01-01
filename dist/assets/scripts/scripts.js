536ec150412ae3c85b23655daf2fde5edb126f80
amirmohammad <zangiabadi1378888@gmail.com>
2022-01-01 12:20:07 +0330

    create header &navbar

---
+++ dist/assets/scripts/scripts.js
@@ -0,0 +1,22 @@
+let hamburgerPage = document.querySelector(".hamburger-page");
+let hamburgerPageList = document.querySelector(".hamburger-page .list");
+
+let closePage = document.querySelector(".close");
+let openPage = document.querySelector(".hamburger-menu label");
+
+function setHamburgerPage() {
+  closePage.style.display = "flex";
+  openPage.style.display = "none";
+  hamburgerPage.style.transform= "translate(0 , 0 )";
+  hamburgerPage.style.width = "100%";
+  hamburgerPage.style.height = "100vh";
+  hamburgerPageList.style.display = "block"
+}
+function closeHamburgerPage() {
+  closePage.style.display = "none";
+  openPage.style.display = "flex";
+  hamburgerPage.style.width = "0";
+  hamburgerPage.style.height = "0";
+  hamburgerPage.style.transform= "translate(-100% , -100%)";
+  hamburgerPageList.style.display = "none"
+}
